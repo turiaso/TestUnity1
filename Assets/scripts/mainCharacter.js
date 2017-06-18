4ab295bc8c75ca8f5a02e10b71efa795b2bf5a4b
DESKTOP-U854SK8\JuaN_HeAvY <juan.ruizpachon@gmail.com>
2017-06-18 14:51:32 +0200

    first commit

---
+++ Assets/scripts/mainCharacter.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+private var status: int = 0;
+
+var speed: float = 1.0;
+
+function Start () {
+	
+}
+
+function Update () {
+
+    switch(status){
+        case 1:
+            transform.Translate(0,0,speed * Time.deltaTime);
+            break;
+        case -1:
+            transform.Translate(0,0,-speed * Time.deltaTime);
+            break;
+    }
+	
+}
+
+function OnGUI() {
+    var e : Event = Event.current;
+    if (e.isKey) {
+        Debug.Log("Current event detected: " + e.type + "Detected key code: " + e.keyCode);
+    }
+
+    switch(e.type){
+        case EventType.KeyDown:
+            switch(e.keyCode){
+                case  KeyCode.UpArrow:
+                    status = 1;
+                    break;
+                case  KeyCode.DownArrow:
+                    status = -1;
+                    break;
+            }
+            break;
+        case EventType.KeyUp:
+            status = 0;
+            break;
+    }
+}
